09f82e4f0bd0bc44d16f269bcf8d416a096e53ff
Yuki <yuki@c-10-253-251-239.clients.eduroam.liu.se>
2020-09-30 17:35:19 +0200

    working shiny app

--- R/main.R
+++ R/main.R
@@ -1,6 +1,7 @@
 library(jsonlite)
 library(shiny)
 library(shinythemes)
+library(ggplot2)
 
 
 api_key = "AP643htGoOq3MVvE72ypY7B0Q5hsnfeln7ocXHWP"
@@ -12,58 +13,32 @@ get_data <- function(endpoint) {
 }
 
 data = get_data(mars_weather_url)
+temp_av <- c()
+temp_min <- c()
+temp_max <- c()
 
-ui <- fluidPage(
-  theme = shinytheme("lumen"),
-  titlePanel("Mars Weather"),
-  sidebarLayout(
-    sidebarPanel(
-      
-      # Select type of trend to plot
-      selectInput(inputId = "type", label = strong("Select"),
-                  choices = unique(trend_data$type),
-                  selected = "Temperature")
-      )
-    ),
-    
-    # Output: Description, lineplot, and reference
-    mainPanel(
-      plotOutput(outputId = "lineplot", height = "300px"),
-      textOutput(outputId = "desc"),
-      tags$a(href = "https://api.nasa.gov/assets/insight/InSight%20Weather%20API%20Documentation.pdf", "Source: NASA API", target = "_blank")
-    )
-)
+ws_av <- c()
+ws_min <- c()
+ws_max <- c()
+ws_dir <- c()
 
-
-
-server <- function(input, output) {
-    # data[["648"]][["AT"]][["av"]]
-    temp_av <- c()
-    temp_min <- c()
-    temp_max <- c()
-    
-    ws_av <- c()
-    ws_min <- c()
-    ws_max <- c()
-    ws_dir <- c()
-    
-    press_av <- c()
-    press_min <- c()
-    press_max <- c()
-    for (i in 1:length(data[["sol_keys"]])){
-      temp_av[i] <- data[[data[["sol_keys"]][i]]][["AT"]][["av"]]
-      temp_min[i] <- data[[data[["sol_keys"]][i]]][["AT"]][["mn"]]
-      temp_max[i] <- data[[data[["sol_keys"]][i]]][["AT"]][["mx"]]
-      
-      ws_av[i] <- data[[data[["sol_keys"]][i]]][["HWS"]][["av"]]
-      ws_min[i] <- data[[data[["sol_keys"]][i]]][["HWS"]][["mn"]]
-      ws_max[i] <- data[[data[["sol_keys"]][i]]][["HWS"]][["mx"]]
-      # ws_dir <- c()
-      
-      press_av[i] <- data[[data[["sol_keys"]][i]]][["PRE"]][["av"]]
-      press_min[i] <- data[[data[["sol_keys"]][i]]][["PRE"]][["mn"]]
-      press_max[i] <- data[[data[["sol_keys"]][i]]][["PRE"]][["mx"]]
-    }
+press_av <- c()
+press_min <- c()
+press_max <- c()
+for (i in 1:length(data[["sol_keys"]])){
+  temp_av[i] <- data[[data[["sol_keys"]][i]]][["AT"]][["av"]]
+  temp_min[i] <- data[[data[["sol_keys"]][i]]][["AT"]][["mn"]]
+  temp_max[i] <- data[[data[["sol_keys"]][i]]][["AT"]][["mx"]]
+  
+  ws_av[i] <- data[[data[["sol_keys"]][i]]][["HWS"]][["av"]]
+  ws_min[i] <- data[[data[["sol_keys"]][i]]][["HWS"]][["mn"]]
+  ws_max[i] <- data[[data[["sol_keys"]][i]]][["HWS"]][["mx"]]
+  # ws_dir <- c()
+  
+  press_av[i] <- data[[data[["sol_keys"]][i]]][["PRE"]][["av"]]
+  press_min[i] <- data[[data[["sol_keys"]][i]]][["PRE"]][["mn"]]
+  press_max[i] <- data[[data[["sol_keys"]][i]]][["PRE"]][["mx"]]
+}
 frame <- data.frame("days" = data[["sol_keys"]], 
                     "av temp" = temp_av, 
                     "min temp" = temp_min, 
@@ -74,26 +49,41 @@ frame <- data.frame("days" = data[["sol_keys"]],
                     "av pres" = press_av,
                     "min pres" = press_min,
                     "max pres" = press_max
-                    ) 
-  
-# Histogram of the Old Faithful Geyser Data ----
-# with requested number of bins
-# This expression that generates a histogram is wrapped in a call
-# to renderPlot to indicate that:
-#
-# 1. It is "reactive" and therefore should be automatically
-#    re-executed when inputs (input$bins) change
-# 2. Its output type is a plot
-output$lineplot <- renderPlot({
-  
-  x    <- faithful$waiting
-  bins <- seq(min(x), max(x), length.out = input$bins + 1)
-  
-  hist(x, breaks = bins, col = "#75AADB", border = "white",
-       xlab = "Waiting time to next eruption (in mins)",
-       main = "Histogram of waiting times")
-})
+) 
+
+ui <- fluidPage(
+  theme = shinytheme("lumen"),
+  titlePanel("Mars Weather"),
+  sidebarLayout(
+    sidebarPanel(
+      
+      # Select type of trend to plot
+      selectInput(inputId = "selected", label = strong("Select"),
+                  choices = c("Temperature" = "temp",
+                              "Windspeed" = "wind",
+                              "Pressure" = "pres"),
+                  selected = "Temperature")
+    ),
+    
+    # Output: Description, lineplot, and reference
+    mainPanel(
+      plotOutput(outputId = "lineplot", height = "300px"),
+      textOutput(outputId = "desc"),
+      tags$a(href = "https://api.nasa.gov/assets/insight/InSight%20Weather%20API%20Documentation.pdf", "Source: NASA API", target = "_blank")
+    )
+  )
+)
+
+# Define server function
+server <- function(input, output) {
+  output$lineplot <- renderPlot({
+    ggplot(frame, aes(x=days, group = 1)) + 
+      geom_line(aes(y = .data[[paste0("av.", input$selected)]]), color = "green") + 
+      geom_line(aes(y = .data[[paste0("min.", input$selected)]]), color="blue") +
+      geom_line(aes(y = .data[[paste0("max.", input$selected)]]), color="red")
+    })
 }
 
-
+# Create Shiny object
+shinyApp(ui = ui, server = server)
 
